7ec774e11118b98fee7eff0f53714bc6934643cb
paulofernando <pf@paulofernando.net.br>
2016-10-23 15:00:38 -0300

    Title in Movie details is being shown just when reach 100% collapsed.

--- app/src/main/java/br/net/paulofernando/moviest/view/activity/MovieDetailsActivity.java
+++ app/src/main/java/br/net/paulofernando/moviest/view/activity/MovieDetailsActivity.java
@@ -64,8 +64,7 @@ import rx.schedulers.Schedulers;
 
 import static br.net.paulofernando.moviest.util.NetworkUtils.INTERNET_CHECK_TIME;
 
-public class MovieDetailsActivity extends AppCompatActivity implements YouTubeThumbnailView.OnInitializedListener,
-        AppBarLayout.OnOffsetChangedListener {
+public class MovieDetailsActivity extends AppCompatActivity implements YouTubeThumbnailView.OnInitializedListener {
 
     public static final int DETAILS_POSTER_SIZE_INDEX = 3;
     public static final int DETAILS_BACKDROP_SIZE_INDEX = 4;
@@ -103,11 +102,8 @@ public class MovieDetailsActivity extends AppCompatActivity implements YouTubeTh
     private String baseImageUrl = "http://image.tmdb.org/t/p/" + TMDB.POSTER_W780;
     private Tracker mTracker;
     private static final int PAGE_NAME = R.string.name_activity_movie_details;
-    private boolean mIsTheTitleVisible = false;
     private ArrayList<String> imagesMap = new ArrayList<>();
 
-    private static final float PERCENTAGE_TO_SHOW_TITLE_AT_TOOLBAR = 1.0f;
-    private static final int ALPHA_ANIMATIONS_DURATION = 200;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -147,11 +143,15 @@ public class MovieDetailsActivity extends AppCompatActivity implements YouTubeTh
             int scrollRange = -1;
 
             @Override
-            public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
+            public void onOffsetChanged(AppBarLayout appBarLayout, int offset) {
+                int maxScroll = appBarLayout.getTotalScrollRange();
+                float percentage = (float) Math.abs(offset) / (float) maxScroll;
+
                 if (scrollRange == -1) {
                     scrollRange = appBarLayout.getTotalScrollRange();
                 }
-                if (scrollRange + verticalOffset < 20) {
+
+                if (percentage == 1.0f) {
                     collapseToolbar.setTitle(movie.title + (!movie.releaseDate.equals("") ?
                             " (" + movie.releaseDate.substring(0, 4) + ")" : ""));
                     isShow = true;
@@ -178,36 +178,6 @@ public class MovieDetailsActivity extends AppCompatActivity implements YouTubeTh
         super.onResume();
         loadImages();
         sendAnalyticsInfo();
-    }
-
-    @Override
-    public void onOffsetChanged(AppBarLayout appBarLayout, int offset) {
-        int maxScroll = appBarLayout.getTotalScrollRange();
-        float percentage = (float) Math.abs(offset) / (float) maxScroll;
-
-        handleToolbarTitleVisibility(percentage);
-    }
-
-    private void handleToolbarTitleVisibility(float percentage) {
-        if (percentage >= PERCENTAGE_TO_SHOW_TITLE_AT_TOOLBAR) {
-            if(!mIsTheTitleVisible) {
-                startAlphaAnimation(titleTextView, ALPHA_ANIMATIONS_DURATION, View.VISIBLE);
-                mIsTheTitleVisible = true;
-            }
-        } else {
-            if (mIsTheTitleVisible) {
-                startAlphaAnimation(titleTextView, ALPHA_ANIMATIONS_DURATION, View.INVISIBLE);
-                mIsTheTitleVisible = false;
-            }
-        }
-    }
-
-    public static void startAlphaAnimation (View v, long duration, int visibility) {
-        AlphaAnimation alphaAnimation = (visibility == View.VISIBLE)
-                ? new AlphaAnimation(0f, 1f) : new AlphaAnimation(1f, 0f);
-        alphaAnimation.setDuration(duration);
-        alphaAnimation.setFillAfter(true);
-        v.startAnimation(alphaAnimation);
     }
 
     private void sendAnalyticsInfo() {
